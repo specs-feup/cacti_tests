10ef1d234c366092447f461977ed0c6b97d37da6
Fabio Morais <fabiocunhamorais23@gmail.com>
2023-04-28 11:46:53 +0100

    Now accepts standards

--- report.py
+++ report.py
@@ -1,5 +1,14 @@
 import json
 import os
+from enum import Enum
+
+class standards(Enum):
+    stand_11 = 1
+    stand_17 = 2
+    stand_20 = 3
+    stand_98 = 4
+    extensions = 5
+
 
 
 def latexTest(string):
@@ -22,17 +31,32 @@ def latexBool(bool):
         latexBool= r"\textcolor{red}{False}"
         return latexBool
 
+def getStand(stand):
+    if stand.name == "C++98":
+        return 1
+    elif stand.name == "C++11":
+        return 2
+    elif stand.name == "C++17":
+        return 3
+    elif stand.name == "C++20":
+        return 4
+
+class Standard:
+    def __init__(self, name, result):
+        self.name = name
+        self.result = result 
+
+
 class Result:
     def __init__(self, name, tests):
         self.name = name
         self.tests= tests
 class Test:
-    def __init__(self, name, success, time, tries=1):
+    def __init__(self, name, success, time=0, tries=1):
         self.name = name
         self.success = success
         self.time = time
-        if tries !=1 :
-            self.tries= tries
+        self.tries= tries
 
 if __name__ == '__main__':
     root_dir = './'
@@ -42,7 +66,7 @@ if __name__ == '__main__':
     f = open("report.tex", "w")
 
     # different test types folders
-    general_path = os.path.join(root_dir, "output")
+    general_path = os.path.join(root_dir, "output/")
 
     # write usepackages and title to the tex file
     f.write(r"\documentclass{article}"+"\n"+r"\usepackage{booktabs}"+"\n"+r"\usepackage{xltabular}"+"\n")
@@ -51,75 +75,80 @@ if __name__ == '__main__':
     f.write(r"\title{Clava Testing Results}"+"\n"+r"\maketitle"+"\n"+r"\newcolumntype{Y}{>{\centering\arraybackslash}X}"+"\n")
    
    
-    
+    standards= []
     results= []
     
+    for standard_spec in os.listdir(general_path):              # standard folders
+        print(standard_spec)
+        general_path = os.path.join(general_path, standard_spec)   
+        for general_type in os.listdir(general_path):           # cpp reference categories
+            general_path = os.path.join(general_path, general_type)
+            for type_tests in os.listdir(general_path):         # cpp reference subcategories
+                type_path = os.path.join(general_path, type_tests)
+                for indiv_tests in os.listdir(type_path):       # multiple source files
+                    indiv_path = os.path.join(type_path, indiv_tests)
+                    for filename in os.listdir(indiv_path):
+                        if filename.endswith('.json'):          # loops over files to find JSON files
+                            json_path = os.path.join(indiv_path, filename)    
+                            with open(json_path) as json_file:  # reads the JSON file
+                                parsed_json= json.load(json_file)
+                                name = ""
+                                time = ""
+                                tests= []
+                                for key,value in parsed_json.items():
+                                    if key == "name":
+                                        name = parsed_json[key]
+                                    elif key == "test_idempotency":
+                                        test = Test(key, parsed_json[key]["success"])
+                                        tests.append(test)
+                                    else:
+                                        if (parsed_json[key]["success"]):
+                                            test = Test(key, parsed_json[key]["success"], parsed_json[key]["time"]) 
+                                        else:
+                                            test = Test(key, parsed_json)
+                                        tests.append(test)
 
-    for general_type in os.listdir(general_path):
-        general_path = os.path.join(general_path, general_type)
-        # loop for different folders
-        for type_tests in os.listdir(general_path):
-            type_path = os.path.join(general_path, type_tests)
-        
-            # loops over the various individual folders
-            for indiv_tests in os.listdir(type_path):
-                indiv_path = os.path.join(type_path, indiv_tests)
-            
-                # loops over files to find JSON files
-                for filename in os.listdir(indiv_path):
-                    if filename.endswith('.json'):
-                        json_path = os.path.join(indiv_path, filename)    
-
-                        # reads the JSON file
-                        with open(json_path) as json_file:
-                            parsed_json= json.load(json_file)
-
-                        name = ""
-                        time = ""
-                        tests= []
-                
-                        for key,value in parsed_json.items():
-                            if key == "name":
-                                name = parsed_json[key]
-                            else:
-                                test = Test(key, parsed_json[key]["success"], parsed_json[key]["time"]) 
-                                tests.append(test)
-
-                        result = Result(name, tests)
-                        results.append(result)
-
-
-    # sort tests since they are not granted to be in order
-    results.sort(key=lambda x:x.name)
+                                result = Result(name, tests)
+                                results.append(result)
+        # sort tests since they are not granted to be in order
+        results.sort(key=lambda x:x.name)
+        stand = Standard(standard_spec, results)
+        standards.append(stand)
+        general_path = os.path.join(root_dir, "output")
     
-    # start table with a column for source file's name and 2 columns per test  
-    f.write(r"\begin{xltabular}{\textwidth}{l")
-    for x in range(len(results[0].tests)):
-        f.write("cc")
-    f.write(r"}"+ "\n"+(r"\toprule")+"\n")
+    standards.sort(key=lambda x: getStand(x))
 
-    #column with source file's name
-    f.write(r"\multicolumn{1}{Y}{}"+"\n")
+    for standard in standards:
+        f.write(r"\section{"+ standard.name + r"}"+"\n")
+        # start table with a column for source file's name and 2 columns per test  
+        f.write(r"\begin{xltabular}{\textwidth}{l")
+        for x in range(len(results[0].tests)):
+            f.write("cc")
+        f.write(r"}"+ "\n"+(r"\toprule")+"\n")
 
-    for test in results[0].tests:
-        f.write(r"& \multicolumn{2}{Y}{\textbf{"+ "{0}".format(latexTest(test.name))+ r"}}")
+        #column with source file's name
+        f.write(r"\multicolumn{1}{Y}{}"+"\n")
 
-    f.write(r"\\"+"\n")
-    f.write(r"\cmidrule{2-"+str(2*len(results[0].tests)+1)+r"}")
+        for test in results[0].tests:
+            f.write(r"& \multicolumn{2}{Y}{\textbf{"+ "{0}".format(latexTest(test.name))+ r"}}")
 
-    for test in results[0].tests:
-        f.write(r"&Time&Success")
-    f.write(r"\\"+"\n")
-    f.write(r"\midrule"+"\n")
+        f.write(r"\\"+"\n")
+        f.write(r"\cmidrule{2-"+str(2*len(results[0].tests)+1)+r"}")
 
+        for test in results[0].tests:
+            f.write(r"&Time&Success")
+        f.write(r"\\"+"\n")
+        f.write(r"\midrule"+"\n")
+        f.write(r"\endhead")
 
-    # writing result rows
-    for result in results:
-        row= r"\textbf{" + latexSource(result.name) + r"}"
-        for test in result.tests:
-           row+= r'& {0}&{1}'.format(test.time, latexBool(test.success))
-        row += r' \\[0.5ex]'
-        f.write(row+"\n")
-    f.write(r"\bottomrule"+"\n")
-    f.write(r"\end{xltabular}"+"\n"+r"\end{document}")
+        # writing result rows
+        for result in results:
+            row= r"\textbf{" + latexSource(result.name) + r"}"
+            for test in result.tests:
+                row+= r'& {0}&{1}'.format(test.time, latexBool(test.success))
+            row += r' \\[0.5ex]'
+            f.write(row+"\n")
+        f.write(r"\bottomrule"+"\n")
+        f.write(r"\end{xltabular}"+"\n")
+    f.write(r"\end{document}")
   

1fb6b4c2537b023ef0107e5c043f9fb5171ec32c
fprada <fmamp24@gmail.com>
2023-07-12 17:59:41 +0100

    started refactoring tests

--- aux_scripts/src.cpp
+++ aux_scripts/src.cpp
@@ -1,5 +1,6 @@
-int main(){
-   __array_rank(int[10][20]) == 2;
-   __array_extent(int, 1)    == 20;
-   return 0;
+int main() {
+    int a = 10;
+    int b = 20;
+    int max = (a > b) ? a : b;
+    return 0;
 }

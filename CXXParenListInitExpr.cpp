ce03ad6e671ab5128b6380edd4a470a29075fdcf
fprada <fmamp24@gmail.com>
2023-07-13 18:32:08 +0100

    updated

--- CXXParenListInitExpr.cpp
+++
@@ -1,8 +0,0 @@
-struct A {
-  int a;
-  double b;
-};
- 
-int main() {
-  A a(1, 1.0);
-}
--- UnnamedGlobalConstantDec.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <source_location>
-#include <string_view>
-
-int main() {
-    std::source_location b;
-}

e7c273fb3c5c3074078e22c5fddd9c0cae9b19ee
fprada <fmamp24@gmail.com>
2023-07-18 14:04:48 +0100

    generating list for keywords but not for nodes

--- aux_scripts/metadata_gen.py
+++ aux_scripts/metadata_gen.py
@@ -22,11 +22,12 @@ def extract_indentation_block(lines, search_string):
 def execute_clang(directory):
     language = "C++" if directory == "C++" else "C"
     standard_list = os.listdir(directory)
-    
+    standard_list = [standard for standard in standard_list if "extensions" not in standard.lower() and "23" not in standard]
+        
     # Iterate over all standards
     for standard in standard_list:
         standard_path = os.path.join(directory, standard)
-
+        print("standard_path: " + standard_path)
         # Getting the standard name
         standard_match = re.search(r'([^\\/]+)$', standard_path)
         standard_name = standard_match.group(1)
@@ -37,27 +38,30 @@ def execute_clang(directory):
         # Iterate over all folders
         for folder in folder_list:
             folder_path = os.path.join(standard_path, folder)
-
+            print("folder_path: " + folder_path)
             keyNodes = os.listdir(folder_path)
 
             for keyNode in keyNodes:
                 keyNode_path = os.path.join(folder_path, keyNode)
+                print("keyNode_path: " + keyNode_path)
                 # List of files
-                src_file_list = os.listdir(folder_path)
+                src_file_list = os.listdir(keyNode_path)
                 
                 # Iterate over all files
                 for src_file in src_file_list:
-                    src_file_path = os.path.join(folder_path, src_file)
+                    src_file_path = os.path.join(keyNode_path, src_file)
+                    print("file_path: " + src_file_path)
                     command = ['clang', f'--std={standard_name}', '-Xclang', '-ast-dump', '-S', src_file_path]
                     output = subprocess.check_output(command).decode('utf-8')
                     source_string = "src.c" if language == "C" else "src.cpp"
+                    print("source " + source_string)
                     file_translation_unit = "\n".join(extract_indentation_block(output.split("\n"), source_string))
                     keywords = re.findall(r'\b\w+(?:Expr|Decl|Operator|Literal|Cleanups|Stmt)\b', file_translation_unit)
                     keywords = set((keyword[3:] if keyword.find("3") != -1 else keyword) for keyword in keywords)
                     sorted_keywords = json.dumps(sorted(keywords), indent=4)
 
                     # Save keywords to "keywords.txt" file
-                    with open(os.path.join(folder_path, "keywords.txt"), "w") as file:
+                    with open(os.path.join(keyNode_path, "keywords.txt"), "w") as file:
                         file.write(sorted_keywords)
 
 if __name__ == '__main__':
